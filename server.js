757e84eadf3a059f4ec50ec6ed952adb979c8c8e
Devin Lumley <keiroshin@gmail.com>
2015-03-12 20:50:43 -0700

    Converting webserver to node, small style changes

---
+++ server.js
@@ -0,0 +1,17 @@
+var express     = require('express');
+
+var app = express()
+
+app.set('port', (process.env.PORT || 3000));
+app.set('views', __dirname + '/public');
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  res.render('index.ejs');
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
